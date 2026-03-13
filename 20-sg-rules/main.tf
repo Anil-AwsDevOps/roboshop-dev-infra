91578b62cb0b440470ae0b3eb582813102137814
Anil <anilbeeraka99@gmail.com>
2026-03-13 21:19:31 +0530

    Terraform infrastructure setup

---
+++ 20-sg-rules/main.tf
@@ -0,0 +1,45 @@
+# trunk-ignore(checkov/CKV_AWS_23)
+resource "aws_security_group_rule" "bastion_internet" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  #cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = [local.my_ip]
+  # which sg you are creating this rule
+  security_group_id = local.bastion_sg_id
+}
+
+# trunk-ignore(checkov/CKV_AWS_23)
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.bastion_sg_id
+  # which sg you are creating this rule
+  security_group_id = local.mongodb_sg_id
+}
+# trunk-ignore(checkov/CKV_AWS_23)
+resource "aws_security_group_rule" "mongodb_catalogue" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.catalogue_sg_id
+  # which sg you are creating this rule
+  security_group_id = local.mongodb_sg_id
+}
+
+resource "aws_security_group_rule" "mongodb_user" {
+  type              = "ingress"
+  from_port         = 27017
+  to_port           = 27017
+  protocol          = "tcp"
+  #where traffic is coming from
+  source_security_group_id = local.user_sg_id
+  # which sg you are creating this rule
+  security_group_id = local.mongodb_sg_id
+}
