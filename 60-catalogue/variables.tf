9d26a638df91f44b91afeee54089c78e6db0c0ef
Anil <anilbeeraka99@gmail.com>
2026-03-22 15:35:59 +0530

    aws

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z07884672JQBJJ77AF6DD"
+}
+
+variable "domain_name" {
+  default = "anilbeeraka.online"
+}
+
+variable "app_version" {
+    default = "v3"
+}
