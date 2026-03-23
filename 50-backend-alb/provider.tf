f91a6f90f999126e83bb5d39728da8e4f69afc3c
Anil <anilbeeraka99@gmail.com>
2026-03-23 12:54:00 +0530

    a

---
+++ 50-backend-alb/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws" # terraform aws provider version
+      version = "6.33.0"
+    }
+  }
+  backend "s3" {
+    bucket = "anil-remotes3"
+    key    = "roboshop-anil-backend-alb"
+    region = "us-east-1"
+    encrypt = true
+    #use_lockfile = true
+  }
+
+}
+
+provider "aws" {
+ region = "us-east-1"
+}
---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z07884672JQBJJ77AF6DD"
+}
+
+variable "domain_name" {
+  default = "anilbeeraka.online"
+}
